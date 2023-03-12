4ae936ce0b358f50470e62a4ecf0a09c26e05021
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-03-12 15:49:57 +0530

    js

---
+++ demo.js
@@ -0,0 +1,11 @@
+let username = prompt('username pls');
+if (username=== 'jojo'){
+    let pass= prompt('PAssword?');
+    if (pass === 'jojo'){
+        alert('welcome')
+    }
+    else{
+        alert('wrong')
+    }
+}
+
