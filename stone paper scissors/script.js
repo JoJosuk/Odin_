20bfc923593a63c090ef334731e71263a04b95ff
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-03-14 19:46:54 +0530

    stone paper scissors 3

--- stone paper scissors/script.js
+++ stone paper scissors/script.js
@@ -1,8 +1,15 @@
 
 const buttons = document.querySelectorAll('.btn');
-const para =document.querySelector('p');
+const para =document.querySelector('#main');
+const para3 = document.querySelector('dup2')
+const para2=document.querySelector('#dup');
 let choice;
+
 let intcho;
+let compcho;
+
+let playscore;
+let compscore;
 buttons.forEach((button)=>{button.addEventListener('click',updatebutton)})
 
 
@@ -23,5 +30,25 @@ function updatebutton(){
     playgame();
 
 }
-function playgame()
+function playround(){
+    let win =[[0,2,1],[1,0,2],[2,1,0]];
+}
+function playgame(){
+    para.textContent = 'Choose rock,paper or scissors'
+    playround();
+    if (playscore ==5){
+        para2.textContent= 'you won'
+        playscore=0;
+        compscore=0;
+
+    }
+    else if(compscore ==5){
+        para2.textContent ='You lost'
+        playscore=0;
+        compscore=0;
+
+    }
+
+
+}
 console.log('hi')
