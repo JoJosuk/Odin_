27996f2b6cbf76cfbd656b88fdc051a89eec6d3c
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-03-19 19:53:28 +0530

    idk dom ig

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hi");
+const some = document.querySelector(".hey");
+some.style.cssText = "margin:2rem; background: white;";
+some.setAttribute("style", "background:green;");
+console.log(some.classList);
+const somer = document.querySelector("ul");
+console.log(somer.previousElementSibling);
+some.setAttribute('id','idli');
+some.textContent='glory to be'
